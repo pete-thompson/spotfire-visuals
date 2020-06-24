82cb539200a1f583dd0847e86be3d7654d47365b
Peter Thompson <pete.thompson@quintiles.com>
2020-06-24 11:26:26 +0100

    Add support for circular Sankey, plus animated Sankey

--- visuals/JSVizSankey.js
+++ visuals/JSVizSankey.js
@@ -1,3 +1,4 @@
+/* eslint-disable compat/compat */
 // Visualise using a Sankey Chart
 // Note that the d3-sankey library doesn't support IE, so we're not going to support IE either
 
@@ -6,12 +7,19 @@ import { event as currentEvent } from 'd3-selection'
 var _ = require('underscore')
 var JSVizHelper = require('../lib/JSVizHelper.js')
 
-var d3 = _.extend({}, require('d3-array'), require('d3-color'), require('d3-collection'), require('d3-drag'), require('d3-format'), require('d3-path'), require('d3-sankey'), require('d3-scale'), require('d3-selection'), require('d3-shape'), require('d3-zoom'), require('d3-scale-chromatic'))
+var d3 = _.extend({}, require('d3-array'), require('d3-color'), require('d3-collection'), require('d3-format'), require('d3-path'), require('d3-sankey'), require('d3-sankey-circular'), require('d3-scale'), require('d3-selection'), require('d3-shape'), require('d3-zoom'), require('d3-scale-chromatic'))
 
 var $ = require('jquery')
 
 var parent
 var svg
+var nodeGroup
+var linkGroup
+var gradientDefs
+var nodeData = []
+var linkData = []
+// eslint-disable-next-line no-undef
+var linkUniqueIds = new Map()
 var margin = { left: 10, right: 10, top: 10, bottom: 10 }
 var size = { width: 0, height: 0 }
 
@@ -24,13 +32,13 @@ var defaultConfig = {
   nodeWidth: 15,
   nodePadding: 10,
   nodeAlign: 'sankeyJustify',
-  allowNodeDrag: false,
   colourFromColumn: 0,
   colourToColumn: 1,
   sortNodesByValue: true,
   sortNodesReverse: false,
   sortFromColumn: 0,
-  sortToColumn: 1
+  sortToColumn: 1,
+  allowCircular: false
 }
 
 JSVizHelper.SetupViz({
@@ -98,6 +106,12 @@ JSVizHelper.SetupViz({
       tab: 'Axes'
     },
     {
+      caption: 'Allow circular links (appearance will change, sorting isn\'t supported)',
+      type: 'checkbox',
+      name: 'allowCircular',
+      tab: 'Appearance'
+    },
+    {
       caption: 'Node alignment',
       type: 'select',
       name: 'nodeAlign',
@@ -132,16 +146,11 @@ JSVizHelper.SetupViz({
       }
     },
     {
-      caption: 'Allow node dragging',
-      type: 'checkbox',
-      name: 'allowNodeDrag',
-      tab: 'Appearance'
-    },
-    {
       caption: 'Sort nodes by value',
       type: 'checkbox',
       name: 'sortNodesByValue',
-      tab: 'Node ordering'
+      tab: 'Node ordering',
+      disabledIfChecked: 'allowCircular'
     },
     {
       caption: 'Reverse sort?',
@@ -181,6 +190,14 @@ function firstTimeSetup (data, config) {
     .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
     .style('isolation', 'isolate')
+
+  nodeGroup = svg.append('g')
+
+  linkGroup = svg.append('g')
+    .attr('fill', 'none')
+    .attr('stroke', '#000')
+
+  gradientDefs = svg.append('defs')
 }
 
 // Main render method
@@ -198,17 +215,23 @@ function render (data, config) {
   // Update the default fill color (particularly for text) in case theme changes
   svg.attr('fill', $('body').css('color'))
 
-  // Clear out any previous chart
-  svg.selectAll('*').remove()
-
   // Create a data array for nodes
   var anyMarked = false
-  var nodeData = _.reduce(data.data, function (result, d) {
+
+  // Firstly, we clear attributes that may have changed from the previous set of nodes
+  nodeData.map((node) => {
+    node.marked = false
+    node.dataIds = []
+    node.foundInNewData = false
+  })
+
+  nodeData = _.reduce(data.data, function (result, d) {
     if ((d.items[2] || 0) > 0) {
       for (var inOut = 0; inOut < 2; inOut++) {
         var newNode = {}
         _.extend(newNode, {
           id: d.items[inOut],
+          foundInNewData: true,
           tooltip: config.tooltipFormat,
           label: config.labelFormat,
           dataIds: [d.hints.index],
@@ -225,8 +248,14 @@ function render (data, config) {
         newNode.label = newNode.label.replace('%n', newNode.id)
         var existing = _.findWhere(result, { id: newNode.id })
         if (existing) {
+          // Merge data into the existing node
+          existing.foundInNewData = true
+          existing.tooltip = newNode.tooltip
           existing.dataIds.push(newNode.dataIds[0])
+          existing.label = newNode.label
           existing.marked = existing.marked || newNode.marked
+          existing.colour = newNode.colour
+          existing.sort = newNode.sort
         } else {
           result.push(newNode)
         }
@@ -234,6 +263,8 @@ function render (data, config) {
     }
     return result
   }, [])
+  // Now remove any nodes that are no longer present in the data
+  nodeData = nodeData.filter((node) => node.foundInNewData)
 
   // Check for an empty visual
   if (nodeData.length === 0) {
@@ -242,8 +273,20 @@ function render (data, config) {
   }
 
   // Create an array in the right format for links
-  var linkData = _.reduce(data.data, function (result, d) {
-    var newLink = { source: _.findWhere(nodeData, { id: d.items[0] }), target: _.findWhere(nodeData, { id: d.items[1] }), value: (d.items[2] || 0), dataId: d.hints.index, marked: d.hints.marked }
+  linkData.map((link) => { link.foundInNewData = false })
+  linkData = _.reduce(data.data, function (result, d) {
+    const from = d.items[0]
+    const to = d.items[1]
+
+    var newLink = {
+      id: from + '>' + to,
+      source: _.findWhere(nodeData, { id: from }),
+      target: _.findWhere(nodeData, { id: to }),
+      value: (d.items[2] || 0),
+      foundInNewData: true,
+      dataId: d.hints.index,
+      marked: d.hints.marked
+    }
     // Only add the link if both nodes are in the data (to support filtering on Nodes within Spotfire)
     if ((newLink.source !== undefined) && (newLink.target !== undefined) && (newLink.value > 0)) {
       anyMarked = anyMarked || newLink.marked
@@ -251,10 +294,31 @@ function render (data, config) {
       _.each(d.items, function (value, index) {
         newLink.tooltip = newLink.tooltip.replace('%' + index, value)
       })
-      result.push(newLink)
+
+      const existing = result.find(({ source, target }) => source === newLink.source && target === newLink.target)
+      if (existing) {
+        // Update an existing element rather than build a new one
+        existing.value = newLink.value
+        existing.foundInNewData = true
+        existing.dataId = newLink.dataId
+        existing.marked = newLink.marked
+        existing.tooltip = newLink.tooltip
+      } else {
+        result.push(newLink)
+      }
     }
     return result
   }, [])
+
+  // Now remove any links that are no longer present in the data
+  linkData = linkData.filter((link) => link.foundInNewData)
+
+  // generate link Ids for DOM use
+  linkData.map((link) => {
+    if (!linkUniqueIds.has(link.id)) {
+      linkUniqueIds.set(link.id, linkUniqueIds.size)
+    }
+  })
 
   // Grab unique values used for colouring
   var colourDomain = _.uniq(_.map(nodeData, d => d.colour))
@@ -264,15 +328,16 @@ function render (data, config) {
   }
 
   // Now draw the charts
-  var sankey = d3.sankey()
+  var sankey = (config.allowCircular ? d3.sankeyCircular() : d3.sankey())
     .nodes(nodeData)
     .links(linkData)
     .nodePadding(config.nodePadding)
     .nodeWidth(config.nodeWidth)
     .size([size.width, size.height])
     .nodeAlign(d3[config.nodeAlign])
+    .iterations(32)
 
-  if (config.sortNodesByValue) {
+  if (config.sortNodesByValue && !config.allowCircular) {
     sankey.nodeSort((a, b) => {
       var answer
       if (typeof a.sort === 'string') {
@@ -285,99 +350,146 @@ function render (data, config) {
     })
   }
 
-  var graph = sankey()
+  // Get the graph and set up transitions to be synched
+  const graph = sankey()
+  const mainTransition = d3.select('#js_chart').transition('main').duration(1300)
+  const markingTransition = d3.select('#js_chart').transition('marking').duration(200)
 
   // Handle colour gradients
   if (config.linkColour === 'inputToOutput') {
-    var gradient = svg.append('defs')
-      .selectAll('.linkGradient')
-      .data(graph.links)
-      .join('linearGradient')
-      .attr('id', d => 'link-gradient-' + d.index)
+    const gradient = gradientDefs.selectAll('.linkGradient')
+      .data(graph.links, (d) => d.id)
+
+    gradient.exit().remove()
+
+    const gradientStop0 = gradient.select('.linkGradientStop0')
+    const gradientStop100 = gradient.select('.linkGradientStop100')
+
+    const gradientEnter = gradient.enter()
+      .append('linearGradient')
+      .attr('class', 'linkGradient')
+      .attr('id', d => 'link-gradient-' + linkUniqueIds.get(d.id))
       .attr('gradientUnits', 'userSpaceOnUse')
+
+    gradient.merge(gradientEnter)
+      .transition(mainTransition)
       .attr('x1', d => d.source.x1)
       .attr('x2', d => d.target.x0)
 
-    gradient.append('stop')
+    const gradientStop0Enter = gradientEnter.append('stop')
+      .attr('class', 'linkGradientStop0')
       .attr('offset', '0%')
+
+    gradientStop0.merge(gradientStop0Enter)
+      .transition(mainTransition)
       .attr('stop-color', d => colour(d.source.colour))
 
-    gradient.append('stop')
+    const gradientStop100Enter = gradientEnter.append('stop')
+      .attr('class', 'linkGradientStop100')
       .attr('offset', '100%')
+
+    gradientStop100.merge(gradientStop100Enter)
+      .transition(mainTransition)
       .attr('stop-color', d => colour(d.target.colour))
   }
 
   // add in the links
-  var link = svg.append('g')
-    .attr('fill', 'none')
-    .attr('stroke', '#000')
-    .selectAll('.link')
-    .data(graph.links)
-    .join('path')
+  const link = linkGroup.selectAll('.link')
+    .data(graph.links, (d) => d.id)
+
+  link.exit()
+    .transition(mainTransition)
+    .style('stroke-width', 0)
+    .remove()
+
+  // New links start at width 0 and grow out
+  const linkEnter = link.enter().append('path')
     .attr('class', 'link')
-    .attr('stroke-opacity', (d) => !anyMarked ? 0.4 : d.marked ? 0.4 : 0.1)
-    .style('mix-blend-mode', 'multiply')
-    .attr('d', d3.sankeyLinkHorizontal())
-    .style('stroke-width', d => d.width)
-    .attr('stroke', d => config.linkColour === 'none' ? '#aaa'
-      : config.linkColour === 'inputToOutput' ? 'url(#link-gradient-' + d.index + ')'
-        : config.linkColour === 'input' ? colour(d.source.colour)
-          : colour(d.target.colour))
-    .sort((a, b) => b.width - a.width)
+    .style('stroke-width', 0)
     .on('click', (selected) => {
       currentEvent.stopPropagation()
       window.markIndices({ markMode: JSVizHelper.getMarkMode(currentEvent), indexSet: [selected.dataId] })
     })
 
+  // Handle marking
+  link.merge(linkEnter)
+    .transition(markingTransition)
+    .attr('stroke-opacity', (d) => !anyMarked ? 0.4 : d.marked ? 0.4 : 0.1)
+
+  // Animate the rest of the attributes
+  link.merge(linkEnter)
+    .transition(mainTransition)
+    .attr('d', d => config.allowCircular ? d.path : d3.sankeyLinkHorizontal()(d))
+    .style('stroke-width', d => d.width)
+
+  // attributes that we don't want to animate
+  link.merge(linkEnter)
+    .style('mix-blend-mode', 'multiply')
+    .attr('stroke', d => config.linkColour === 'none' ? '#aaa'
+      : config.linkColour === 'inputToOutput' ? 'url(#link-gradient-' + d.index + ')'
+        : config.linkColour === 'input' ? colour(d.source.colour)
+          : colour(d.target.colour))
+    .sort((a, b) => b.width - a.width)
+
   // Add hover text
-  link.append('title')
+  const linkTitleEnter = linkEnter.append('title')
+  link.select('title').merge(linkTitleEnter)
     .text(d => d.tooltip.replace('%v', d3.format(config.valueFormat)(d.value)))
 
   // add in the nodes
-  var node = svg.append('g')
-    .selectAll('.node')
-    .data(graph.nodes)
-    .enter().append('g')
-    .attr('class', 'node')
-    .attr('transform', d => 'translate(' + d.x0 + ',' + d.y0 + ')')
+  const node = nodeGroup.selectAll('.node')
+    .data(graph.nodes, (d) => d.id)
 
-  if (config.allowNodeDrag) {
-    node.call(d3.drag()
-      .subject(d => d)
-      .on('start', function () { this.parentNode.appendChild(this) }) // Make sure the node appears on top of others
-      .on('drag', function (d) {
-        // the function for moving the nodes - keeping within the boundary of the chart
-        d.screenHeight = d.screenHeight || (d.y1 - d.y0)
-        d3.select(this)
-          .attr('transform', 'translate(' + d.x0 + ',' + (d.y0 = Math.max(0, Math.min(size.height - d.screenHeight, currentEvent.y))) + ')')
-        sankey.update(graph)
-        link.attr('d', d3.sankeyLinkHorizontal())
-      }))
-  }
+  node.exit().remove()
+
+  const nodeEnter = node.enter().append('g')
+    .attr('class', 'node')
+    .attr('transform', d => 'translate(' + (d.x0 + d.x1) / 2 + ',' + (d.y0 + d.y1) / 2 + ')')
+    .attr('height', 0)
+
+  node.merge(nodeEnter)
+    .transition(mainTransition)
+    .attr('transform', d => 'translate(' + d.x0 + ',' + d.y0 + ')')
+    .attr('height', d => d.y1 - d.y0)
 
   // add the rectangles for the nodes
-  node
+  const nodeRect = node.select('rect')
+  const nodeRectEnter = nodeEnter
     .append('rect')
-    .attr('height', d => d.y1 - d.y0)
     .attr('width', sankey.nodeWidth())
-    .style('fill', d => colour(d.colour))
+
+  nodeRect.merge(nodeRectEnter)
+    .transition(markingTransition)
     .attr('fill-opacity', (d) => !anyMarked ? 1.0 : d.marked ? 1.0 : 0.2)
     .attr('stroke-opacity', (d) => !anyMarked ? 1.0 : d.marked ? 1.0 : 0.2)
-    .style('stroke', d => d3.rgb(colour(d.colour)).darker(2))
+
+  nodeRect.merge(nodeRectEnter)
     .attr('data-id-array', d => JSON.stringify(d.dataIds))
-  // Add hover text
-    .append('title')
-    .text(d => d.tooltip.replace('%v', d3.format(config.valueFormat)(d.value)))
+    .transition(mainTransition)
+    .attr('width', sankey.nodeWidth())
+    .attr('height', d => d.y1 - d.y0)
+    .attr('width', sankey.nodeWidth())
+    .style('fill', d => d3.rgb(colour(d.colour)))
+    .style('stroke', d => d3.rgb(colour(d.colour)).darker(2))
 
   // add in the title for the nodes
-  node.append('text')
-    .attr('x', -6)
-    .attr('y', d => (d.y1 - d.y0) / 2)
+  const nodeText = node.select('text')
+
+  const nodeTextEnter = nodeEnter.append('text')
     .attr('dy', '.35em')
-    .attr('text-anchor', 'end')
     .attr('transform', null)
+    .style('cursor', 'default')
+    .attr('x', d => (d.x0 < size.width / 2) ? 6 + sankey.nodeWidth() : -6)
+
+  nodeText.merge(nodeTextEnter)
+    .attr('text-anchor', d => (d.x0 < size.width / 2) ? 'start' : 'end')
     .text(d => d.label.replace('%v', d3.format(config.valueFormat)(d.value)))
-    .filter(d => d.x0 < size.width / 2)
-    .attr('x', 6 + sankey.nodeWidth())
-    .attr('text-anchor', 'start')
+    .transition(mainTransition)
+    .attr('y', d => (d.y1 - d.y0) / 2)
+    .attr('x', d => (d.x0 < size.width / 2) ? 6 + sankey.nodeWidth() : -6)
+
+  // Add hover text
+  nodeRect.select('title')
+    .merge(nodeRectEnter.append('title'))
+    .text(d => d.tooltip.replace('%v', d3.format(config.valueFormat)(d.value)))
 }
